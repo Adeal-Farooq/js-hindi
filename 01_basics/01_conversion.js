5aca3497edbc4e7b6ae35b25be25beee96bf87b2
Adeal-Farooq <loneadeal@gmail.com>
2025-08-08 09:34:54 +0000

    conversion of datatypes

---
+++ 01_basics/01_conversion.js
@@ -0,0 +1,34 @@
+let score = "33abcC"
+
+// console.log(typeof score)
+// console.log(typeof (score))
+
+let valueInNumber = Number(score)         //use capital 'Number' while conversion
+
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);        //NaN  not a number
+
+// so we get the folloeing results while convrsion
+// "33" => 33
+//"3abc" => NaN
+// true => 1; false => 0
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+//1 => true;  0 => false
+//"" => false
+//"Adeal" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
