04e6a53e57a0747950267b64a907d56ebae3d13a
Adeal-Farooq <loneadeal@gmail.com>
2025-08-08 05:26:13 +0000

    datatyoes

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,33 @@
+"use strict";   // trest all js code as never version
+
+//alert(3 +3)   // we are usimng nodejs not browser
+
+console.log(3 
+    +3)             //it will give output but it bis not better way to write program
+
+    //code readability should be high
+
+console.log("Adeal")
+
+let name = "adeal"
+let age = 18
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint 
+// string => ""
+//boolean => true /false
+//null => standalon value
+//undefined => 
+
+//symbol => used for uniqueness
+
+
+//object
+
+console.log(typeof age);        //number
+console.log(typeof null);       //object
+console.log(typeof undefined);      //undefined
+
+
